fea0d1b4d8780bfbae2c4c12feb7492a172ba5b4
Marcel Muhehe <marcelmuhehe@outlook.com>
2026-03-10 17:05:52 +0300

    M

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -5,6 +6,160 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Image.asset(
+            "assets/background4.jpg",
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          SafeArea(
+            bottom: false,
+            child: 
+          Container(
+              padding: EdgeInsets.symmetric(vertical: 30.0),
+
+              width: double.infinity,
+              
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 20.0),
+                    child: Column(
+
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // SizedBox(height: 10.0,),
+                        Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 40.0),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Welcome back",
+                          style: TextStyle(color: Colors.white,fontSize: 20.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(60.0),
+                          topRight: Radius.circular(60.0),
+                        ),
+                      ),
+                      
+                      
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow:[BoxShadow(color: const Color.fromARGB(255, 2, 39, 4),
+                                  blurRadius: 20,
+                                  offset: Offset(0, 10)
+                              
+                              
+                                  )
+                                  ] 
+                              
+                                ),
+                               child: Column(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      border: Border(bottom: BorderSide(color: Colors.grey))
+                                     
+                                    ),
+                                    child: TextField(
+                                        decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          
+                                          hintText: "Use Email or Username",
+                                          prefixIcon: Icon(Icons.person),
+                                        ),
+                                      ),
+
+                                      
+
+                                  ),
+                                  
+                                   Container(
+                                    decoration: BoxDecoration(
+                                      
+                                    ),
+                                    child: TextFormField(
+                                      obscureText: true,
+                                      obscuringCharacter: '*',
+                                      validator: (value){
+                                        if(value== null || value.isEmpty){
+                                        return "please enter password";
+                                        }
+                                        return null;
+                                      },
+
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          hintText: "Enter password",
+                                          prefixIcon: Icon(
+                                            Icons.password_outlined,
+                                            
+                                          ),
+                                        ),
+
+                                        
+                                      ),
+                                   ),
+                                  
+                                  
+
+                                  
+                                  
+
+                                ],
+
+                               ),
+                               
+                               
+                              ),
+                              
+                            ),
+                            SizedBox(height: 40.0,),
+                            Text("Forgot password?",style: TextStyle(color: Colors.black),),
+                          ],
+                        
+                        
+                          
+                        ),
+                        
+                      ),
+                      
+                    ),
+                    
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+    
   }
 }
--- lib/screens/welcome_back_screen.dart
+++ lib/screens/welcome_back_screen.dart
@@ -1,4 +1,4 @@
-import 'package:firstapp/widget/custom_scaffold.dart';
+
 
 import 'package:flutter/material.dart';
 import 'sign_in_screen.dart';
@@ -9,128 +9,116 @@ class WelcomeBackScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      child: Column(
-
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      extendBodyBehindAppBar: true,
+      body: Stack(
         children: [
-          Flexible(
-            flex: 8,
-            child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20.0,
-              vertical: 0.0
-              
-            ),
-            child: Center(child: RichText(textAlign: TextAlign.center,
-            text: const TextSpan(
+          Image.asset(
+            "assets/background4.jpg",
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          SafeArea(child: 
+          Column(
               children: [
-
-                TextSpan(
-                  text: "Welcome back,\n",
-                  style: TextStyle(
-                    color: Colors.white24,
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w600,
-
-                  )
-
-
-                ),
-                TextSpan(
-                  text: "Enter your employee details",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white24
+                Flexible(
+                  flex: 8,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 0.0,
+                    ),
+                    child: Center(
+                      child: RichText(
+                        textAlign: TextAlign.center,
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Welcome back,\n",
+                              style: TextStyle(
+                                color: Colors.white24,
+                                fontSize: 45.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            TextSpan(
+                              text: "Enter your employee details",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                  
                 ),
-
-
-
-              ]
-            ),
-            )
-            ),
-
-
-          ),
-          
-          ),
-          Flexible(
-            flex: 1,
-            child: Align(
-            alignment: AlignmentGeometry.bottomCenter,
-            child: Row(
-              
-            
-              children: [
-                  Expanded(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        side: const BorderSide(color: Colors.white),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignInScreen(),
+                Flexible(
+                  flex: 1,
+                  child: Align(
+                    alignment: AlignmentGeometry.bottomCenter,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              foregroundColor: Colors.white,
+                              side: const BorderSide(color: Colors.white),
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignInScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text("Sign in"),
                           ),
-                        );
-
-
-                      },
-                      child: const Text("Sign up"),
-                    ),
-                  ), 
-                 
-
-                  const SizedBox(width: 15),
-
-                  Expanded(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        side: const BorderSide(color: Colors.white),
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
                         ),
-                      ),
-                      onPressed: () {
 
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SignUpScreen(),
+                        const SizedBox(width: 15),
+
+                        Expanded(
+                          child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              foregroundColor: Colors.white,
+                              side: const BorderSide(color: Colors.white),
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SignUpScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text("Sign up"),
                           ),
-                        );
-                      },
-                      child: const Text("Sign up"),
+                        ),
+                      ],
                     ),
-                  ), 
-
-
-            
-              
+                  ),
+                ),
               ],
             ),
-          ),
-          
-          )
-
-
-
-
-        ],
-      ),
       
 
-
-    );
+          
+          ),
+        ],
+      ),
+    ); 
   }
 }
